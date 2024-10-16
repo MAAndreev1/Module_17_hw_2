b36997812dce06ab55be3d4e610318beccbedfdc
Михаил Андреев <andreev.mihail.a@gmail.com>
2024-10-16 23:34:40 +0500

    first

---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+from home_work.app.models.user import User
+from home_work.app.models.task import Task
---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from pandantic import BaseModel
+
+
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+
+class UpdateUser(BaseModel):
+    firstname: str
+    lastname: str
+    age: int
+
+
+class CreateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+
+class UpdateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
